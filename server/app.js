fdd92f5f8ef6cca1dea902af092c12133f2ffced
Betsy Rowley <BRowley8@gmail.com>
2017-04-07 20:18:09 -0500

    Spins up server

---
+++ server/app.js
@@ -0,0 +1,29 @@
+var express = require('express');
+var app = express();
+var path = require('path');
+var bodyParser = require('body-parser');
+
+//Database module
+
+//Route Modules
+
+//App Config
+app.set('port', (process.env.PORT || 8000));
+
+//Middleware Conifg
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static('server/public'));
+
+
+//Routes
+app.get('/', function(req, res) {
+  res.sendFile(path.resolve('server/public/views/index.html'));
+});
+
+
+
+//Listen
+app.listen(app.get('port'), function(){
+  console.log('Listening on port: ', app.get('port'));
+});
