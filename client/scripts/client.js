d139fd2e9c64875a362e0a55b9f202ac5dabfc13
Betsy Rowley <BRowley8@gmail.com>
2017-04-08 14:08:48 -0500

    Adds Favorites Controller and http request for API

--- client/scripts/client.js
+++ client/scripts/client.js
@@ -1,38 +1,63 @@
 var movieApp = angular.module('movieApp', []);
 
+//SearchController
 movieApp.controller('SearchController', ['$scope', 'BlockbusterService',
             function($scope, BlockbusterService) {
   console.log('SearchController called');
 
   var blockbusterService = BlockbusterService;
 
-  $scope.movie = {
-    title: '',
-  };
+  $scope.title = '';
 
-  $scope.addMovie = blockbusterService.addMovie;
+  $scope.getOMDB = blockbusterService.getOMDB;
 
-}]);
+}]); //end SearchController
 
+//ResultsController
 movieApp.controller('ResultsController', ['$scope', 'BlockbusterService',
             function($scope, BlockbusterService) {
 
   var blockbusterService = BlockbusterService;
 
-  $scope.movieArray = blockbusterService.movieArray;
-}]);
+  $scope.omdbResults = blockbusterService.omdbResults;
+  $scope.addFavoriteMovie = blockbusterService.addFavoriteMovie;
+}]); //End ResultsController
 
-movieApp.factory('BlockbusterService', [function() {
-  var movieArray = [];
+//FavoritesController
+movieApp.controller('FavoritesController', ['$scope', 'BlockbusterService',
+            function($scope, BlockbusterService) {
 
-  var addMovie = function(newMovie) {
+var blockbusterService = BlockbusterService;
+
+$scope.favoritesArray = blockbusterService.favoritesArray;
+
+}]); //End FavoritesController
+
+//Factory
+movieApp.factory('BlockbusterService', ['$http', function($http) {
+
+  var omdbResults = {};
+
+  var favoritesArray = [];
+
+  var addFavoriteMovie = function(newMovie) {
     var copy = angular.copy(newMovie);
-    movieArray.push(copy);
-    console.log(movieArray);
+    favoritesArray.push(copy);
+    console.log(favoritesArray);
   };
 
   return {
-    movieArray : movieArray,
-    addMovie : addMovie
+    favoritesArray : favoritesArray,
+    addFavoriteMovie : addFavoriteMovie,
+    omdbResults : omdbResults,
+    getOMDB : function(searchTitle) {
+      var movie = angular.copy(searchTitle);
+      console.log(movie);
+      $http.get('http://www.omdbapi.com/?t=' + movie +
+                  '&y&plot=full&r=json').then(function(response) {
+                                                omdbResults.response = response;
+                                                console.log(omdbResults.response);
+      });
+    }
   };
-}]);
+}]); //End Factory
