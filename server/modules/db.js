1a8c7dd821ff1e66388fdf9e44ad5aeae289256c
Betsy Rowley <BRowley8@gmail.com>
2017-04-09 12:52:38 -0500

    Connects to mlab and heroku

--- server/modules/db.js
+++ server/modules/db.js
@@ -1,5 +1,6 @@
 var mongoose = require('mongoose');
-var mongoURI = 'mongodb://localhost:27017/favorites';
+var mongoURI = 'mongodb://betsy:betsy@ds157390.mlab.com:57390/favorite_movies';
+// 'mongodb://localhost:27017/favorites';
 var MongoDB = mongoose.connect(mongoURI).connection;
 
 MongoDB.on('error', function(err) {
